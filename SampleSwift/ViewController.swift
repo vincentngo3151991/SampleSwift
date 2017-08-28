d104a58b6204a8c540afda389cad8b56bd3257ad
Vincent Ngo <vincentngo31051991@yahoo.com>
2017-08-27 22:47:14 -0700

    Initial Commit

---
+++ SampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSwift
+//
+//  Created by Thunderiven on 8/27/17.
+//  Copyright © 2017 Thunderiven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
